f14212a4cfc047c2d0f4ed9f1b7b9908aceacdb3
Ayman AlFarra <aymanelfarra2004@gmail.com>
2025-06-05 09:45:49 +0300

    an edit on the side bar over lay

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 console.log(window.innerWidth);
+
 //sideBar style
 const sideBarCssId = "sidebar-css";
 if (!document.getElementById(sideBarCssId)) {
