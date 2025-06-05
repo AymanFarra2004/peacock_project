6de13cebec041c3a3a051f2e2f89d366d6c72e3d
Ayman AlFarra <aymanelfarra2004@gmail.com>
2025-06-05 10:10:02 +0300

    update on the mouse crusor

--- folders/components/all_works/allWorks.js
+++ folders/components/all_works/allWorks.js
@@ -5,7 +5,7 @@ categoriesAllWorks();
 const mouseMove = document.getElementById("mouse");
 document.addEventListener("mousemove", (e) => {
   mouseMove.style.transform = `translate(${e.clientX - 50}px, ${
-    e.clientY - 50
+    e.clientY - 75
   }px)`;
 });
 
